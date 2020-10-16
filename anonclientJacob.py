cf613f7f992438a19508e4a3719648f96baf2acf
jacobmjohnson1999 <38044877+jacobmjohnson1999@users.noreply.github.com>
2020-10-16 12:57:42 -0500

    Add files via upload

--- anonclientJacob.py
+++ anonclientJacob.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import struct
+import random
 
 #functions
 
@@ -13,6 +14,14 @@ def packThePacket(sNum, aNum, A, S, F):
 	print(packet)
 	return packet
 
+def stopAndWait(mySocket)
+	mySocket.settimeout(currentTime)
+	servMsg = mySocket.recvfrom(bufferSize)
+	while !servMsg :
+		numFails = numFails + 1
+		currentTime = currentTime + min((2**numFails + randomrange(0,100)), maxWait)
+	return servMsg
+
 
 msgFromClient       = "Hello UDP Server"
 
@@ -21,6 +30,12 @@ bytesToSend         = str.encode(msgFromClient)
 serverAddressPort   = ("127.0.0.1", 20001)
 
 bufferSize          = 1024
+
+#Maximum amount of time to wait for stop and wait protocol
+maxWait = 2000
+
+#Number of failed attempts to send/recieve data from server
+numFails = 0
 
 #Sets sequence number to 32 bits
 seqNumber = 12345
@@ -55,3 +70,24 @@ msgFromServer = UDPClientSocket.recvfrom(bufferSize)
 msg = "Message from Server {}".format(msgFromServer[0])
 
 print(msg)
+
+#General structure
+while(1):
+	
+	#Wait for response
+	
+	#Perform the appropriate actions for response
+	
+	#Send the response
+	
+	while(#FIN flag is not set):
+	
+		#Wait for the response
+		
+		#Download response
+		
+		#Send seq and ack
+		
+	
+	
+
