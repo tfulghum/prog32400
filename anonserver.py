f32982897cc461cc86d64c10d51b2554825b07bf
Tyler Fulghum <51171819+tfulghum@users.noreply.github.com>
2020-10-18 16:52:48 -0500

    Add files via upload

--- anonserver.py
+++ anonserver.py
@@ -1,22 +1,50 @@
 import socket
+import struct
+import sys
 
- 
+def packThePacket(sNum, aNum, A, S, F):
+    #This struct needs some work but will otherwise function
+    packer = struct.Struct('>iii')
+    lastLine = A*2*2 + S*2 + F
+    packet = packer.pack(sNum, aNum, lastLine)
+    return packet
 
-localIP     = "127.0.0.1"
+def msgParser(msg):
+    packer = '>iii'
+    unpackedMsg = struct.unpack('>iii', msg)
+    seqNumber = unpackedMsg[0]
+    ackNumber = unpackedMsg[1]
+    flags = unpackedMsg[2]
+    
+    #Determines which flags are set based on the value of the flags variable
+    if flags >= 4:
+        A = 1
+        flags = flags - 4
+    else:
+        A = 0
+    if flags >= 2:
+        S = 1
+        flags = flags - 2
+    else:
+        S = 0
+    if flags >= 1:
+        F = 1
+    else:
+        F = 0
+    
+    return seqNumber, ackNumber, A, S, F
 
-localPort   = 20001
+ #getting command line arguments
+for args in sys.argv:
+    if args == '-p':
+        localPort = int(sys.argv[sys.argv.index(args)+1])
+    if args == '-l':
+        logfile = sys.argv[sys.argv.index(args)+1]
+    if args == '-u':
+        url = sys.argv[sys.argv.index(args)+1]
 
-bufferSize  = 1024
-
- 
-
-msgFromServer       = "Hello UDP Client"
-
-bytesToSend         = str.encode(msgFromServer)
-
- 
-
-# Create a datagram socket
+localIP     = "localhost"
+#socket.gethostbyname(socket.gethostname())
 
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
@@ -26,30 +54,53 @@ UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 UDPServerSocket.bind((localIP, localPort))
 
- 
-
 print("UDP server up and listening")
 
  
 
 # Listen for incoming datagrams
+go = 1
+while(go == 1):
+    try:
+        #First packet headers
+        recvInfo, addr = UDPServerSocket.recvfrom(96)
 
-while(True):
+        seqNumberR, ackNumberR, Ar, Sr, Fr = msgParser(recvInfo)
 
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+        print("Got ", seqNumberR, ackNumberR, Ar, Sr, Fr)
 
-    message = bytesAddressPair[0]
+        tempVar = ackNumberR
 
-    address = bytesAddressPair[1]
+        ackNumberR = seqNumberR + 1
+        seqNumberR = 100
 
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP  = "Client IP Address:{}".format(address)
-    
-    print(clientMsg)
-    print(clientIP)
+        if(Ar == 0):
+            Ar = 1
 
-   
+        print("Sending ", seqNumberR, ackNumberR, Ar, Sr, Fr)
 
-    # Sending a reply to client
+        handshakePack = packThePacket(seqNumberR, ackNumberR, Ar, Sr, Fr)
+        #Send ack back
+        UDPServerSocket.sendto(handshakePack, addr)
 
-    UDPServerSocket.sendto(bytesToSend, address)
+        #Complete handshake
+        recvInfo, addr = UDPServerSocket.recvfrom(96)
+        seqNumberR, ackNumberR, Ar, Sr, Fr = msgParser(recvInfo)
+
+        print("Got ", seqNumberR, ackNumberR, Ar, Sr, Fr)
+        
+        Sr = 0
+        tempVar = ackNumberR
+
+        ackNumberR = seqNumberR + 1
+        seqNumberR = tempVar
+
+        print(seqNumberR, ackNumberR, Ar, Sr, Fr)
+        print("Handshake Complete")
+
+        go = 0
+
+    except KeyboardInterrupt:
+        print("Exiting now...")
+        UDPServerSocket.close()
+        break
